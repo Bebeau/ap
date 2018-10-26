2fc4c6ae9f371dc25be3f234441e3cd5a522035b
Bebeau <kyle@theinitgroup.com>
2018-10-25 20:11:19 -0700

    update

--- server.js
+++ server.js
@@ -5,17 +5,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
 // Serve any static files
 app.use(express.static(path.join(__dirname, 'public')));
 // Handle React routing, return all requests to React app
